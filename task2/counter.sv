7127104752589e9d4cd16b93b1dd552e363ac5ab
ccrownhill <ckronbichler@proton.me>
2023-10-27 11:15:34 +0100

    task2 edits

--- task2/counter.sv
+++ task2/counter.sv
@@ -10,7 +10,7 @@ module counter #(
 
 always_ff @(posedge clk) 
     if (rst) count <= {WIDTH{1'b0}};
-    else if (!en) count <= count - {{WIDTH-1{1'b0}}, 1};
-    else     count <= count  + {{WIDTH-1{1'b0}}, en};
+    else if (!en) count <= count - {{WIDTH-1{1'b0}}, 1'b1};
+    else count <= count  + {{WIDTH-1{1'b0}}, 1'b1};
     
 endmodule
--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -13,8 +13,8 @@ int main(int argc, char **argv, char **env)   {
     // init trace dump
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
-    top -> trace (tfp, 99);
-    tfp -> open ("counter.vcd");
+    top->trace (tfp, 99);
+    tfp->open ("counter.vcd");
 
     // init Vbuddy
     if(vbdOpen() !=1) return(-1);
@@ -22,12 +22,12 @@ int main(int argc, char **argv, char **env)   {
     vbdSetMode(1);
 
     // initialize simulation inputs
-    top ->clk = 1;
-    top ->rst = 1;
-    top ->en = 0;
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
 
     // run simulation for many clock cycles
-    for (i=0; i<300; i++) {
+    for (i=0; i<500; i++) {
 
         // dump variables into VCD file and toggle clock
         for (clk=0; clk<2; clk++) {
@@ -36,14 +36,14 @@ int main(int argc, char **argv, char **env)   {
             top ->eval ();
         }
 
+	top->rst = 0;
         // ++++ Send count value to Vbuddy
         vbdPlot(int(top->count), 0, 255);
         vbdCycle(i+1);
         // ----- end of Vbuddy output section
 
         // change input stimuli
-        top ->rst = (i < 2) | (i == 15);
-        top -> en = vbdFlag();
+        top->en = vbdFlag();
         if (Verilated::gotFinish()) exit(0);
     }
 
